d6bafd86c144eb938396e986901ab4c2f3d96c50
Babneet Singh <sbabneet@ca.ibm.com>
2020-06-22 08:54:51 -0700

    Do not use rebind in StringConcatFactory
    OpenJ9 does not support MethodHandle.rebind. So, its usage has been temporarily
    removed from StringConcatFactory until OpenJ9 supports OpenJDK MethodHandles.
    
    Signed-off-by: Babneet Singh <sbabneet@ca.ibm.com>

--- src/java.base/share/classes/java/lang/invoke/StringConcatFactory.java
+++ src/java.base/share/classes/java/lang/invoke/StringConcatFactory.java
@@ -635,7 +635,7 @@ public final class StringConcatFactory {
             MethodHandle prepend = JLA.stringConcatHelper("prepend",
                     methodType(long.class, long.class, byte[].class,
                             Wrapper.asPrimitiveType(c), String.class));
-            return prepend.rebind();
+            return prepend;
         }
     };
 
@@ -652,7 +652,7 @@ public final class StringConcatFactory {
         public MethodHandle apply(Class<?> c) {
             MethodHandle mix = JLA.stringConcatHelper("mix",
                     methodType(long.class, long.class, Wrapper.asPrimitiveType(c)));
-            return mix.rebind();
+            return mix;
         }
     };
 
@@ -662,7 +662,7 @@ public final class StringConcatFactory {
         if (mh == null) {
             MethodHandle simpleConcat = JLA.stringConcatHelper("simpleConcat",
                     methodType(String.class, Object.class, Object.class));
-            SIMPLE_CONCAT = mh = simpleConcat.rebind();
+            SIMPLE_CONCAT = mh = simpleConcat;
         }
         return mh;
     }
@@ -673,7 +673,7 @@ public final class StringConcatFactory {
         if (mh == null) {
             MethodHandle newString = JLA.stringConcatHelper("newString",
                     methodType(String.class, byte[].class, long.class));
-            NEW_STRING = mh = newString.rebind();
+            NEW_STRING = mh = newString;
         }
         return mh;
     }
@@ -684,7 +684,7 @@ public final class StringConcatFactory {
         if (mh == null) {
             MethodHandle newArrayWithSuffix = JLA.stringConcatHelper("newArrayWithSuffix",
                     methodType(byte[].class, String.class, long.class));
-            NEW_ARRAY_SUFFIX = mh = newArrayWithSuffix.rebind();
+            NEW_ARRAY_SUFFIX = mh = newArrayWithSuffix;
         }
         return MethodHandles.insertArguments(mh, 0, suffix);
     }
